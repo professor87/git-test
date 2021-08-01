874c93642bc312202315ff58d69e9d291ad97421
Parviz <p_mirzayev@yahoo.com>
2021-08-01 22:02:30 +0400

    iki yeni cumle bastirdik ekrana

--- merhaba.java
+++ merhaba.java
@@ -3,8 +3,8 @@ class merhaba{
 public static void main(String [] args){
 
 System.out.println("merhaba dunya");
-
-
+System.out.println("Git cok iyi bir arac");
+System.out.println("Git iyice pekisiyor");
 }
 
 
