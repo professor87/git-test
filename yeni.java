d3be905ff759436c3a0bd5f79841fa3c9e28b883
Parviz <p_mirzayev@yahoo.com>
2021-08-06 14:21:03 +0400

    yeni.java dosyasina ekrana yazi yazdiran ikinci bir satir ekledik

--- yeni.java
+++ yeni.java
@@ -3,6 +3,7 @@ public class yeni{
     public static void main(String [] args){
 
     System.out.println("yeni.java programinda ekrana bir seyler yazdirdik");
+    System.out.println("yeni.java dosyamiza ikinci bir satir ekleyelim");
 
     }
 }
