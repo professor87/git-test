178ce2642490cf5fea43fd2253eee30e7bc30592
Parviz <p_mirzayev@yahoo.com>
2021-08-06 15:42:10 +0400

    yeni.java dosyasina git kursu iyi gidiyor deye yeni bir mesaj eklemis olduk

--- yeni.java
+++ yeni.java
@@ -3,7 +3,7 @@ public class yeni{
     public static void main(String [] args){
 
     System.out.println("yeni.java programinda ekrana bir seyler yazdirdik");
-    System.out.println("yeni.java dosyamiza ikinci bir satir ekleyelim");
+    System.out.println("git kursu iyi gidiyor");
 
     }
 }
